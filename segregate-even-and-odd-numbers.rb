945fc74c3b4429beb0fe4fa278f200e1aa7b9e48
Ghanshyam Anand <ghanshyamanand1989@gmail.com>
2018-02-24 14:13:36 +0530

    segregate-even-and-odd-numbers.rb

--- segregate-even-and-odd-numbers.rb
+++ segregate-even-and-odd-numbers.rb
@@ -8,8 +8,29 @@
 # Output = {12, 34, 8, 90, 45, 9, 3}
 # In the output, order of numbers can be changed, i.e., in the above example 34 can come before 12 and 3 can come before 9.
 
+require 'pry'
+
+def seg_odd_even(input_array, length)
+  left_counter  = 0
+  right_counter = length -1
+  while left_counter < right_counter
+
+    while input_array[left_counter] % 2 == 0 and left_counter < right_counter
+      left_counter = left_counter + 1
+    end
+
+    while input_array[right_counter] % 2 != 0 and left_counter < right_counter
+      right_counter = right_counter - 1
+    end
+
+    if left_counter < right_counter
+      input_array[left_counter], input_array[right_counter] = input_array[right_counter], input_array[left_counter]
+    end
+  end
+  puts input_array
+
+end
 
 
-
-input_array   =  [0, 1, 0, 1, 0, 0, 1, 1, 0, 0]
-seg_0_1_method2(input_array, 10)
+input_array   =  [12, 34, 45, 9, 8, 90, 3]
+seg_odd_even(input_array, 7)
